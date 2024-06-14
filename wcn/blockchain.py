c2cfbd49766030a100f785d9eb3860718dad0bc5
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-06-14 11:58:18 +0800

    Update blockchain.py

--- wcn/blockchain.py
+++ wcn/blockchain.py
@@ -1,1 +1,12 @@
+import hashlib
+from requests import get
 
+class NFT(object):
+  def __init__(self,crypto_type):
+    self.crypto_type = crypto_type
+    self.hash_data = []
+  def hash(data : list):
+    text1 = [data[0],data[1],str(data[2])]
+    t1 = ",".join(text1)
+    hash1 = hashlib.sha256(t1.encode()).hexdigest()
+    return hash1
